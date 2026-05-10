2662b1524d600c65fb8753a68eca23dfbd985e66
ZzzGayane <aleksanyang143@gmail.com>
2026-05-10 22:26:46 +0400

    Update library_item.py

--- src/models/library_item.py
+++ src/models/library_item.py
@@ -1,1 +1,19 @@
+from abc import ABC, abstractmethod
 
+class LibraryItem(ABC):
+  def __init__(self, title, item_id):
+    self.title = title
+    self.item_id = item_id
+    self.available = True
+  def display_info(self):
+    print("Title", self.title)
+    print("Item_ID", self.item_id)
+    print("Available", self.available)
+    
+  @abstractmethod
+  def borrow(self):
+    pass
+
+  @abstractmethod
+  def return_item(self):
+    pass
