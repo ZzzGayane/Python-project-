d96fece22e2d491d7d9455406a1c8504f09a36ab
ZzzGayane <aleksanyang143@gmail.com>
2026-05-10 22:41:04 +0400

    Add DVD class with borrowing and returning functionality

--- src/models/dvd.py
+++ src/models/dvd.py
@@ -1,1 +1,22 @@
+from src.models.library_item import LibraryItem
 
+class DVD(LibraryItem):
+  def __init__(self, title, item_id, duration, genre):
+    super().__init__(title, item_id)
+    self.duration = duration
+    self.genre = genre
+  def display_info(self):
+    super().display_info()
+    print("Duration:", self.duration)
+    print("Genre:", self.genre)
+  def borrow(self):
+    if self.available:
+      self.available = False
+      print(f"{self.title} has been borrowed")
+    else:
+      print(f"{self.title} is already borrowed")
+  def return_item(self):
+    self.available = True
+    print(f"{self.title} has been returned")
+    
+  
